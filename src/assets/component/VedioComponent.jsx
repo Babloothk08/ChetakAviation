04e989fb7139692d6cd3ead74fa84c68996d3124
Babloothk08 <babloothakur42021@gmail.com>
2026-03-02 16:42:22 +0530

    w adjusted

--- src/assets/component/VedioComponent.jsx
+++ src/assets/component/VedioComponent.jsx
@@ -6,7 +6,7 @@ function VideoComponent() {
   return (
     <section className="relative w-full h-screen overflow-hidden">
       <video
-        className="absolute inset-0 w-full  h-[90vh] md:h-[96vh]  object-cover object-center"
+        className="absolute inset-0 w-[95vw]  h-[90vh] md:h-[94vh]  object-cover object-center"
         src={chetakBannerVideo}
         autoPlay
         muted
